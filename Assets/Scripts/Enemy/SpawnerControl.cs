29df7a1d06ba40bbd190d5a52417553432401b6d
hieunq <hieu811@gmail.com>
2021-02-20 11:45:09 +0700

    addd spwaner control

---
+++ Assets/Scripts/Enemy/SpawnerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerControl : MonoBehaviour
+{
+    bool spawn = true;
+    List<AtkSpawner> listSpawner = new List<AtkSpawner>();
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        GameObject[] listObject = GameObject.FindGameObjectsWithTag("atkSpawner");
+        foreach (var obj in listObject)
+        {
+            AtkSpawner spawnerTmp = obj.GetComponent<AtkSpawner>();
+            if (spawnerTmp == null) continue;
+            listSpawner.Add(spawnerTmp);
+        }
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(2, 5));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        if(listSpawner.Count <= 0) { return; }
+        int idxOfSpawner = Random.Range(0, listSpawner.Count - 1);
+        AtkSpawner spawner = listSpawner[idxOfSpawner];
+        spawner.SpawnAttacker(Constant.GetPrefabPath(Constant.LIZZARD_PATH));
+    }
+}
